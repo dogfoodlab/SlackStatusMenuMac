00a89f280b34733c78d71b3e0a06181d7e9ba438
Doggie Foo <dogfooding01@gmail.com>
2018-03-24 12:43:19 +0900

    Add setting window

---
+++ SlackStatusMenuMac/Forms/SettingWindowController.cs
@@ -0,0 +1,58 @@
+﻿using Foundation;
+using AppKit;
+using System;
+using System.Collections.Generic;
+
+namespace SlackStatusMenuMac.Forms
+{
+    public partial class SettingWindowController : NSWindowController
+    {
+        public SettingWindowController(IntPtr handle) : base(handle)
+        {
+        }
+
+        [Export("initWithCoder:")]
+        public SettingWindowController(NSCoder coder) : base(coder)
+        {
+        }
+
+        public SettingWindowController() : base("SettingWindow")
+        {
+        }
+
+        public override void AwakeFromNib()
+        {
+            base.AwakeFromNib();
+
+            var tokens = Slack.TokenUtil.LoadTokens();
+
+            var text = string.Join("\n", tokens) + "\n";
+
+            this.TokenTextView.InsertText(new NSString(text));
+        }
+
+        public new SettingWindow Window
+        {
+            get { return (SettingWindow)base.Window; }
+        }
+
+        partial void OkButton_Click(Foundation.NSObject sender)
+        {
+            var text = this.TokenTextView.String;
+
+            var list = text.Trim().Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var tokens = new List<string>(list);
+
+            Slack.TokenUtil.SaveTokens(tokens);
+
+            this.Close();
+        }
+
+        partial void CancelButton_Click(NSObject sender)
+        {
+            this.Close();
+        }
+
+
+    }
+}
